80ff7b8d8f20fc4c91d0d97cc5cd7cad0ae99f09
asus\user <justtaco0121@gmail.com>
2017-06-05 14:03:15 +0800

    Revert "no message"
    This reverts commit d94cf93bce67d4551d6f70ca94090f5f92408e03.

--- application/models/goods.php
+++ application/models/goods.php
@@ -7,20 +7,15 @@ class goods extends CI_Model
         parent::__construct();
     }
 
-    public function get($limit = 100){
-            return $this->db
-            ->get("goods", $limit)
-            ->result();
-    }
-
-    public function search($_POST){
-        $this->input->post(NULL,TRUE);
+    public function get(){
 
     }
 
-    public function delete($id){
-        $this->db->where('id', $id);
-        $this->db->delete('goods');
+    public function search(){
+
+    }
+
+    public function delete(){
 
     }
 
